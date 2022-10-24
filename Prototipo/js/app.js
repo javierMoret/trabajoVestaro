d82c1c018ccc4519a2cb75991b649877b90cb820
Javier Moret Galán <javier.s257989@cesurformacion.com>
2022-10-24 14:16:33 +0200

    JS Login

--- Prototipo/js/app.js
+++ Prototipo/js/app.js
@@ -1,4 +1,13 @@
 
 function irContactos(){
-    window.location.href = "contactos.html";
+    var username = document.getElementById("usuario").value;
+    var password = document.getElementById("password").value;
+    username=username.toLowerCase();
+    username = username.replace(/\s+/g, '')
+    console.log(password);
+    if(password=="1234" && (username=="javiermoret" || username=="albertofernandez")){
+        window.location.href = "contactos.html";
+    }else{
+        alert("Usuario o contraseña no válido.");
+    }
 }
