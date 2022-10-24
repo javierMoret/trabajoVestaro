749d38d46df639aef49d41e91de9d42b71414ef7
albertofernadez <albertofernandezbaena@outlook.com>
2022-10-24 12:34:39 +0200

    Sobre Nosotros

---
+++ Prototipo/js/custom.js
@@ -0,0 +1,24 @@
+
+  $(window).load(function(){
+    $('.preloader').fadeOut(1000); 
+  });
+
+    function initParallax() {
+    $('#home').parallax("50%", 0.3);
+
+  }
+  initParallax();
+  
+  $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
