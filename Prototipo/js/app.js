8a213cd83fdd64daa9d50c9ac47ee41859879728
Javier Moret Galán <javier.s257989@cesurformacion.com>
2022-11-28 12:42:36 +0100

    Commit sin bootstrap

--- Prototipo/js/app.js
+++ Prototipo/js/app.js
@@ -1,13 +1,35 @@
 
+
+var username;
+
 function irContactos(){
     var username = document.getElementById("usuario").value;
     var password = document.getElementById("password").value;
-    username=username.toLowerCase();
+    username = username.toLowerCase();
     username = username.replace(/\s+/g, '')
     console.log(password);
-    if(password=="1234" && (username=="javiermoret" || username=="albertofernandez")){
+    if(password=="1234" && (username=="javiermoret" || username=="albertofernandez" || username=="franciscoromero")){
         window.location.href = "catalogo.html";
+
     }else{
         alert("Usuario o contraseña no válido.");
     }
 }
+
+var nombrePerfil = document.getElementById("perfil");
+console.log(username2);
+switch(username2){
+    case "javiermoret":
+        nombrePerfil.innerHTML = "Javier Moret";
+        break;
+    case "albertofernandez":
+        nombrePerfil.innerHTML = "Alberto Fernández";
+        break;
+    case "franciscoromero":
+        nombrePerfil.innerHTML = "Francisco Romero";
+        break;
+    default:
+        nombrePerfil.innerHTML = username;
+}
+
+
